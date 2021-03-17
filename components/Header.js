b0b4aa242daa944e07c7a5b83aab9b56c1d04aac
João Fernando Corsini <jf.corsini@gmail.com>
2021-03-17 13:05:16 +0200

    Remove faunaDB image

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,9 @@
-import { headerBanner, headerTitle } from "../styles/header";
+import { headerTitle } from "../styles/header";
 
 export default function Header(props) {
   return (
     <>
-      <a href="https://fauna.com" target="_blank" rel="noopener noreferrer">
-        <img
-          className={headerBanner.className}
-          src="/static/fauna-logo-blue.png"
-          height="35px"
-          width="auto"
-        />
-      </a>
       <h1 className={headerTitle.className}>TourTag</h1>
-      {headerBanner.styles}
       {headerTitle.styles}
     </>
   );
